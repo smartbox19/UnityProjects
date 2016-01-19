29461126de66ad501012998deb9c69d303d42c46
Luke Walden <smartbox19@gmail.com>
2016-01-19 22:14:15 +1100

    Near final commit

---
+++ PauseMenu/Assets/Scripts/Demo.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+this.transform.position.x+=0.5*Time.deltaTime;
+}
--- PauseMenu/Assets/Scripts/PauseMenu.js
+++ PauseMenu/Assets/Scripts/PauseMenu.js
@@ -1,8 +1,14 @@
 ﻿#pragma strict
 var paused = false;
+var style : GUIStyle;
+var pauseBGEditMe : Texture;
+var pauseTitleEditMe : String;
+var pauseTitleColorEditMe : Color;
 
 function Start () {
-	
+	style.fontSize=35;
+	style.normal.textColor = pauseTitleColorEditMe;
+	style.fontStyle = FontStyle.Bold;
 }
 
 function Update () {
@@ -14,8 +20,17 @@ function Update () {
 		}
 	}   
 	if(paused) {
-		Time.timeScale = 0;
+		Time.timeScale=0;
 	} else {
 		Time.timeScale = 1;
+	}
+}
+
+function OnGUI () {
+	if(paused) {
+
+		GUI.DrawTexture(Rect(0,0,Screen.width,Screen.height),pauseBGEditMe);
+		GUI.Label(Rect(35,35,Screen.width/2,128),pauseTitleEditMe,style);
+
 	}
 }
