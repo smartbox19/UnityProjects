ec5062902fe02fa9ca07bb1db5874a9543fd27ea
Luke Walden <smartbox19@gmail.com>
2016-01-19 17:28:00 +1100

    Initial Commit
    Working on a Pause Menu for a friend.

---
+++ PauseMenu/Assets/Scripts/PauseMenu.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+var paused = false;
+
+function Start () {
+	
+}
+
+function Update () {
+	if(Input.GetKeyDown(KeyCode.Escape)) {
+		if(paused) {
+			paused=false;
+		} else {
+			paused=true;
+		}
+	}   
+	if(paused) {
+		Time.timeScale = 0;
+	} else {
+		Time.timeScale = 1;
+	}
+}
